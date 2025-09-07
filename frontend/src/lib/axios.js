6860f45bc95e575a2229b2f2f3e404d23e5aad4e
Anand-Reddy-Yanna <anandreddyyanna5@gmail.com>
2025-09-07 17:33:09 +0530

    small

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL ="https://verbolink.onrender.com";
-// const BASE_URL = import.meta.env.MODE === "development" ? "https://verbolink.onrender.com" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : import.meta.env.VITE_API_URL;
+// const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
